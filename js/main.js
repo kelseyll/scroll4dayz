4473b79efd8214ddbf7433cf4661461d679c751e
kelseyll <kelsey.legault@gmail.com>
2016-06-24 22:44:09 -0700

    Clean up code

---
+++ js/main.js
@@ -0,0 +1,37 @@
+$(function() {
+
+    // Initialize variables
+    var frameNumber = 0,
+    playbackConst = 500,
+    setHeight = document.getElementById("set-height"),
+    vid = document.getElementById('v0');
+
+    // Dynamically set the page height according to video length
+    vid.addEventListener('loadedmetadata', function() {
+        setHeight.style.height = Math.floor(vid.duration) * playbackConst + "px";
+    });
+
+    // Initialize scrollPlay
+    function scollPlay(){
+        var frameNumber  = window.pageYOffset/playbackConst;
+        vid.currentTime  = frameNumber;
+        window.requestAnimationFrame(scollPlay);
+    }
+    window.requestAnimationFrame(scollPlay);
+
+    // Scroll to top at bottom of DOM
+    $(window).on('scroll', function() {
+        scrollPosition = $(window).scrollTop();
+        var domheight = $(window).height();
+        var viewportheight = $(".background").height();
+        var topofpageheight = domheight - viewportheight - 1;
+        if (scrollPosition >= topofpageheight) {
+            $('html, body').animate({scrollTop: '0px'}, 0);
+        }
+
+        // Scroll to bottom at top of DOM
+        else if (scrollPosition <= 0) {
+            $('html, body').animate({scrollTop: topofpageheight}, 0);
+        }
+    });
+});
